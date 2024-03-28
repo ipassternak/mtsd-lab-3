d1ce2132c967e4ffde172b9b7894277d005ca2a5
Ivan Pasternak <retriumpf@gmail.com>
2024-03-28 18:50:37 +0200

    Update Python project source code

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from enum import Enum
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +8,35 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+MAX_MATRIX_SIZE = 20
+
+class MatrixOperation(Enum):
+    add = 'addition'
+    sub = 'subtraction'
+    mult = 'multiplication'
+    div = 'division'
+    dot = 'product'
+
+
+@router.get('/matrix')
+def matrix(size: int = 10, op: MatrixOperation = MatrixOperation.dot) -> dict:
+    size = max(1, min(size, MAX_MATRIX_SIZE))
+    mx_a = np.random.rand(size, size)
+    mx_b = np.random.rand(size, size)
+    if op == MatrixOperation.add:
+        res = mx_a + mx_b
+    elif op == MatrixOperation.sub:
+        res = mx_a - mx_b
+    elif op == MatrixOperation.mult:
+        res = mx_a * mx_b
+    elif op == MatrixOperation.div:
+        res = mx_a / mx_b
+    elif op == MatrixOperation.dot:
+        res = np.dot(mx_a, mx_b)
+    return {
+        'matrix_a': mx_a.tolist(),
+        'matrix_b': mx_b.tolist(),
+        op.value: res.tolist()
+    }
